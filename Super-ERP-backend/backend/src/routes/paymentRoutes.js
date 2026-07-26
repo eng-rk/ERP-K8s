e24d930868925c47eb95ab939f1019cc5d96d66b
rk30207241300666@depi.eui.edu.eg <rk30207241300666@depi.eui.edu.eg>
2026-07-26 11:28:00 +0300

    add

--- Super-ERP-backend/backend/src/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPublicOfferByToken, processPublicPayment } = require('../controllers/paymentController');
-
-// Public payment page endpoints (no auth required)
-router.get('/:token', getPublicOfferByToken);
-router.post('/:token', processPublicPayment);
-
-module.exports = router;
