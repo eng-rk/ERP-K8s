7aade2d5ecfcd65d454f4c1d4bbe77d6721dddf0
Rowan <rowan4598@gmail.com>
2026-08-23 14:20:21 +0300

    refactor(crm): colocate booking routes

---
+++ Super-ERP-backend/src/modules/crm/bookings/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getBookings, getBookingByRef, updateBooking } = require('../../../controllers/bookingController');
+const { protect } = require('../../../middleware/auth');
+const { checkPermission } = require('../../../middleware/authorize');
+router.get('/', protect, checkPermission('crm.customers.view'), getBookings);
+router.get('/:ref', protect, checkPermission('crm.customers.view'), getBookingByRef);
+router.put('/:id', protect, checkPermission('crm.customers.edit'), updateBooking);
+module.exports = router;
