61496ff36687752add7315364a4b0a8d50d66806
Rowan <rowan4598@gmail.com>
2026-08-23 19:40:24 +0300

    refactor(backend): route payroll through domain controller

--- Super-ERP-backend/src/modules/hrm/features/payroll/routes.js
+++ Super-ERP-backend/src/modules/hrm/features/payroll/routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const { protect } = require('../../../../middleware/auth');
 const { checkPermission } = require('../../../../middleware/authorize');
-const controller = require('../../../../controllers/hrmController');
+const controller = require('./controller');
 
 const router = express.Router();
-
 router.post('/contracts/salary-components', protect, checkPermission('hrm.contracts.edit_salary_components'), controller.upsertSalaryComponent);
 router.delete('/contracts/salary-components/:id', protect, checkPermission('hrm.contracts.edit_salary_components'), controller.deleteSalaryComponent);
 router.put('/contracts/salary/:id', protect, checkPermission('hrm.contracts.edit_net_salary'), controller.updateNetSalaryOnly);
-
 module.exports = router;
