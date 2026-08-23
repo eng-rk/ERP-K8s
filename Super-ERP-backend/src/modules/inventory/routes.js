10f819e1872a0e1c8b5a68d0d22baf9727487a18
Rowan <rowan4598@gmail.com>
2026-08-23 14:21:38 +0300

    refactor(inventory): delegate shipping and transfer routes

--- Super-ERP-backend/src/modules/inventory/routes.js
+++ Super-ERP-backend/src/modules/inventory/routes.js
@@ -3,33 +3,26 @@ const router = express.Router();
 const { protect } = require('../../middleware/auth');
 const { checkPermission } = require('../../middleware/authorize');
 const {
-  postGoodsIssue, createStockTransfer, postInventoryAdjustment,
-  createShipment, createCycleCount, createPhysicalInventory,
+  postInventoryAdjustment, createShipment, createCycleCount, createPhysicalInventory,
   getWarehouses, createWarehouse, updateWarehouse,
   getLots, getSerials, approveAdjustment, rejectAdjustment,
-  getShipments, createTransfer, getTransfers,
   createAdjustment, getAdjustments, getCycleCounts, getPhysicalInventories,
-  createPickTask, getPickTasks, getPickTask, updatePickTask, releasePickWave,
   getInventoryValuation, getABCClassification, getDeadStockReport,
   getReorderAlerts, getExpiryAlerts, getPutawaySuggestion
 } = require('../../controllers/inventoryController');
 const { getValuationDetailed } = require('../../controllers/inventoryExtensionsController');
 const itemRoutes = require('./items/routes');
 const receivingRoutes = require('./receiving/routes');
-
+const shippingRoutes = require('./shipping/routes');
+const transferRoutes = require('./transfers/routes');
 router.use(protect);
 router.use(itemRoutes);
 router.use(receivingRoutes);
-
-router.post('/issues/goods/:id', checkPermission('wms.shipping.post_issue'), postGoodsIssue);
-router.post('/transfers/:id/execute', checkPermission('wms.transfers.execute'), createStockTransfer);
+router.use(shippingRoutes);
+router.use('/transfers', transferRoutes);
 router.post('/adjustments/:id/post', checkPermission('wms.adjustments.approve'), postInventoryAdjustment);
 router.post('/adjustments/:id/approve', checkPermission('wms.adjustments.approve'), approveAdjustment);
 router.post('/adjustments/:id/reject', checkPermission('wms.adjustments.reject'), rejectAdjustment);
-router.post('/shipments', checkPermission('wms.shipping.create_wave'), createShipment);
-router.get('/shipments', checkPermission('wms.shipping.view'), getShipments);
-router.post('/transfers', checkPermission('wms.transfers.request'), createTransfer);
-router.get('/transfers', checkPermission('wms.items.view'), getTransfers);
 router.post('/adjustments', checkPermission('wms.adjustments.create_request'), createAdjustment);
 router.get('/adjustments', checkPermission('wms.adjustments.view'), getAdjustments);
 router.post('/cycle-counts', checkPermission('wms.audits.cycle_count'), createCycleCount);
@@ -41,11 +34,6 @@ router.post('/warehouses', checkPermission('wms.items.create'), createWarehouse)
 router.put('/warehouses/:id', checkPermission('wms.items.edit'), updateWarehouse);
 router.get('/lots', checkPermission('wms.items.view'), getLots);
 router.get('/serials', checkPermission('wms.items.view'), getSerials);
-router.post('/pick-tasks', checkPermission('wms.shipping.create_wave'), createPickTask);
-router.get('/pick-tasks', checkPermission('wms.shipping.view'), getPickTasks);
-router.get('/pick-tasks/:id', checkPermission('wms.shipping.view'), getPickTask);
-router.put('/pick-tasks/:id', checkPermission('wms.shipping.assign_picker'), updatePickTask);
-router.post('/pick-wave/release', checkPermission('wms.shipping.create_wave'), releasePickWave);
 router.get('/reports/valuation', checkPermission('wms.items.view'), getInventoryValuation);
 router.get('/reports/valuation-detailed', checkPermission('wms.items.view'), getValuationDetailed);
 router.get('/reports/abc', checkPermission('wms.items.view'), getABCClassification);
@@ -53,5 +41,4 @@ router.get('/reports/dead-stock', checkPermission('wms.items.view'), getDeadStoc
 router.get('/alerts/reorder', checkPermission('wms.items.view'), getReorderAlerts);
 router.get('/alerts/expiry', checkPermission('wms.items.view'), getExpiryAlerts);
 router.get('/putaway/suggest', checkPermission('wms.items.view'), getPutawaySuggestion);
-
 module.exports = router;
