bd5fb38a6cb1856771ada07b22c4868cfc630351
Rowan <rowan4598@gmail.com>
2026-08-23 14:23:08 +0300

    refactor(crm): colocate analytics routes

---
+++ Super-ERP-backend/src/modules/crm/analytics/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getSystemAnalytics, getMarketingPerformance } = require('../../../controllers/analyticsController');
+const { protect } = require('../../../middleware/auth');
+const { checkPermission } = require('../../../middleware/authorize');
+router.get('/', protect, checkPermission('iam.audit.view'), getSystemAnalytics);
+router.get('/marketing-performance', protect, checkPermission('marketing.campaigns.view'), getMarketingPerformance);
+module.exports = router;
