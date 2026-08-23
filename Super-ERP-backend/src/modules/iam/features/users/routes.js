e84396c7f51945cfa58dbd51043aee7326529f62
Rowan <rowan4598@gmail.com>
2026-08-23 19:51:06 +0300

    refactor(iam): route users through module controller

--- Super-ERP-backend/src/modules/iam/features/users/routes.js
+++ Super-ERP-backend/src/modules/iam/features/users/routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const { protect } = require('../../../../middleware/auth');
 const { checkPermission } = require('../../../../middleware/authorize');
-const controller = require('../../../../controllers/permissionController');
-
+const controller = require('./controller');
 const router = express.Router();
 router.use(protect);
 router.get('/users/:id/permissions', checkPermission('iam.users.view_sensitive'), controller.getUserEffectivePermissions);
