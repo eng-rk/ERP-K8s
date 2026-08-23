94fdb911f77cf8260d43b5cc563927e86e9e3cf5
Rowan <rowan4598@gmail.com>
2026-08-23 13:34:10 +0300

    refactor(frontend): preserve AUX polling with Redux adapter

--- Super-ERP-frontend/src/context/AuxContext.jsx
+++ Super-ERP-frontend/src/context/AuxContext.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchAuxConfig, fetchMyAuxPlan, fetchTeamAux } from '../store/slices/auxSlice';
+
 // Backward-compatible import surface during the Redux migration.
 // AUX state and async operations now live in Redux Toolkit.
 export { useAux } from '../store/hooks';
 export { AUX_COLORS, AUX_ICONS, DEFAULT_AUX_LIST } from '../store/slices/auxSlice';
 
-export const AuxProvider = ({ children }) => children;
+export const AuxProvider = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAuxConfig());
+    dispatch(fetchTeamAux());
+    dispatch(fetchMyAuxPlan());
+    const interval = setInterval(() => dispatch(fetchTeamAux()), 30000);
+    return () => clearInterval(interval);
+  }, [dispatch]);
+
+  return children;
+};
