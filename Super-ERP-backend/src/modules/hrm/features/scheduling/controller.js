bc37ac477e43aa23c9c2d6a27533b7ddcd021048
Rowan <rowan4598@gmail.com>
2026-08-23 19:49:17 +0300

    refactor(hrm): isolate scheduling compatibility handlers

--- Super-ERP-backend/src/modules/hrm/features/scheduling/controller.js
+++ Super-ERP-backend/src/modules/hrm/features/scheduling/controller.js
@@ -1,12 +1,12 @@
-// HRM Scheduling / AUX domain HTTP controller.
-const legacy = require('../../../../controllers/hrmController');
+const handlers = require('../../legacyHandlers');
+
 module.exports = {
-  getDetailedSchedule: legacy.getDetailedSchedule,
-  updateDetailedSchedule: legacy.updateDetailedSchedule,
-  updateAuxStatus: legacy.updateAuxStatus,
-  getTeamAux: legacy.getTeamAux,
-  updateRtmFlag: legacy.updateRtmFlag,
-  getAuxReport: legacy.getAuxReport,
-  upsertAuxSchedule: legacy.upsertAuxSchedule,
-  getAuxSchedules: legacy.getAuxSchedules,
+  getDetailedSchedule: handlers.getDetailedSchedule,
+  updateDetailedSchedule: handlers.updateDetailedSchedule,
+  updateAuxStatus: handlers.updateAuxStatus,
+  getTeamAux: handlers.getTeamAux,
+  updateRtmFlag: handlers.updateRtmFlag,
+  getAuxReport: handlers.getAuxReport,
+  upsertAuxSchedule: handlers.upsertAuxSchedule,
+  getAuxSchedules: handlers.getAuxSchedules,
 };
