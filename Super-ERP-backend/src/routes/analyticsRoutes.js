85cea6b9855559447e44158c25ed5b5b6ef51282
Rowan <rowan4598@gmail.com>
2026-08-23 14:23:49 +0300

    refactor(crm): delegate analytics route to module

--- Super-ERP-backend/src/routes/analyticsRoutes.js
+++ Super-ERP-backend/src/routes/analyticsRoutes.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { getSystemAnalytics, getMarketingPerformance } = require('../controllers/analyticsController');
-const { protect } = require('../middleware/auth');
-const { checkPermission } = require('../middleware/authorize');
-
-router.get('/', protect, checkPermission('iam.audit.view'), getSystemAnalytics);
-router.get('/marketing-performance', protect, checkPermission('marketing.campaigns.view'), getMarketingPerformance);
-
-module.exports = router;
+module.exports = require('../modules/crm/analytics');
