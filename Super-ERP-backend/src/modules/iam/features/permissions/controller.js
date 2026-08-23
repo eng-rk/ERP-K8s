f7caf702a839be6f9327edddbcea93e3893491c7
Rowan <rowan4598@gmail.com>
2026-08-23 19:50:35 +0300

    refactor(iam): isolate permission controller boundary

---
+++ Super-ERP-backend/src/modules/iam/features/permissions/controller.js
@@ -0,0 +1,4 @@
+const legacy = require('../../../../controllers/permissionController');
+exports.getAllPermissions = legacy.getAllPermissions;
+exports.getPermissionTree = legacy.getPermissionTree;
+exports.getPermissionById = legacy.getPermissionById;
