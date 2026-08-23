e73da473d90a5fcc1c87a95c16d577df61760346
Rowan <rowan4598@gmail.com>
2026-08-23 14:13:39 +0300

    refactor(backend): make CRM bookings route module self-contained

--- Super-ERP-backend/src/modules/crm/bookings/routes.js
+++ Super-ERP-backend/src/modules/crm/bookings/routes.js
@@ -1,2 +1,9 @@
-// Compatibility-preserving CRM Bookings route boundary.
-module.exports = require('../../../routes/bookingRoutes');
+const express = require('express');
+const router = express.Router();
+const { getBookings, getBookingByRef, updateBooking } = require('../../../controllers/bookingController');
+const { protect } = require('../../../middleware/auth');
+const { checkPermission } = require('../../../middleware/authorize');
+router.get('/', protect, checkPermission('crm.customers.view'), getBookings);
+router.get('/:ref', protect, checkPermission('crm.customers.view'), getBookingByRef);
+router.put('/:id', protect, checkPermission('crm.customers.edit'), updateBooking);
+module.exports = router;
