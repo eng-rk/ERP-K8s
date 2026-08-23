c503cb7d629f34a976501e227a837c19753ac2a3
Rowan <rowan4598@gmail.com>
2026-08-23 19:05:59 +0300

    refactor(frontend): route HRM exports through feature boundaries

--- Super-ERP-frontend/src/modules/hrm/index.js
+++ Super-ERP-frontend/src/modules/hrm/index.js
@@ -1,7 +1,6 @@
-export { default as HrmDashboardPage } from '../../pages/hrm/HrmDashboardPage';
-export { default as PersonalPage } from '../../pages/hrm/PersonalPage';
-export { default as PayrollPage } from '../../pages/hrm/PayrollPage';
-export { default as TrainingPage } from '../../pages/hrm/TrainingPage';
-export { default as TalentAcquisitionPage } from '../../pages/hrm/TalentAcquisitionPage';
-export { default as PartnershipsPage } from '../../pages/hrm/PartnershipsPage';
-export { default as AuxSchedulePage } from '../../pages/hrm/AuxSchedulePage';
+export { HrmDashboardPage, PersonalPage } from './features/employees';
+export { PayrollPage } from './features/payroll';
+export { TrainingPage } from './features/training';
+export { TalentAcquisitionPage } from './features/talent';
+export { PartnershipsPage } from './features/partnerships';
+export { AuxSchedulePage } from './features/scheduling';
