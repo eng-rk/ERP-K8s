35ef76a76c82c02b97bcd760469240b4e29d3f11
Rowan <rowan4598@gmail.com>
2026-08-23 19:31:36 +0300

    refactor(backend): route legacy products entrypoint through module

--- Super-ERP-backend/src/routes/productRoutes.js
+++ Super-ERP-backend/src/routes/productRoutes.js
@@ -1,17 +1,2 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { checkPermission } = require('../middleware/authorize');
-const {
-  getProducts,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-} = require('../controllers/productController');
-
-router.get('/', protect, checkPermission('wms.items.view'), getProducts);
-router.post('/', protect, checkPermission('wms.items.create'), createProduct);
-router.put('/:id', protect, checkPermission('wms.items.edit'), updateProduct);
-router.delete('/:id', protect, checkPermission('wms.items.delete'), deleteProduct);
-
-module.exports = router;
+// Compatibility entrypoint: Products is implemented under modules/products.
+module.exports = require('../modules/products/routes');
