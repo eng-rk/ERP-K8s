fda03d3910c340d75a9c2c5b18dde6d456373c62
Rowan <rowan4598@gmail.com>
2026-08-23 19:50:44 +0300

    refactor(iam): isolate user authorization controller boundary

---
+++ Super-ERP-backend/src/modules/iam/features/users/controller.js
@@ -0,0 +1,4 @@
+const legacy = require('../../../../controllers/permissionController');
+exports.getUserEffectivePermissions = legacy.getUserEffectivePermissions;
+exports.updateUserCustomPermissions = legacy.updateUserCustomPermissions;
+exports.updateUserRoles = legacy.updateUserRoles;
