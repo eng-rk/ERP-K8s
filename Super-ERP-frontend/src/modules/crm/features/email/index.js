85e9812dad9b4d63050caa8b122d5c81e6e098e7
Rowan <rowan4598@gmail.com>
2026-08-23 13:44:35 +0300

    refactor(frontend): define CRM email feature boundary

---
+++ Super-ERP-frontend/src/modules/crm/features/email/index.js
@@ -0,0 +1,3 @@
+export { default as EmailsPage } from '../../../pages/EmailsPage';
+export { default as EmailComposer } from '../../../pages/EmailComposer';
+export { default as SentEmailsPage } from '../../../pages/SentEmailsPage';
